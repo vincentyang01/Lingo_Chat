07ea22e930f60aefceac0f20071744f19ae88a97
vincentyang01 <vincentyangcsi@gmail.com>
2020-09-24 23:25:04 -0400

    Working on the backend calls

--- src/Chat.js
+++ src/Chat.js
@@ -4,20 +4,30 @@ import { useParams } from "react-router-dom"
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined"
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
 import db from './firebase.js'
+import Message from "./Message"
+import ChatInput from "./ChatInput"
 
 function Chat() {
     const { roomId } = useParams();
     const [roomDetails, setRoomDetails] = useState(null)
+    const [roomMessages, setRoomMessages] = useState([])
 
     useEffect(() => {
         if(roomId){
             db.collection('rooms')
             .doc(roomId)
-            .onSnapshot(snapshot => (setRoomDetails(snapshot.data())
-            ))
+            .onSnapshot(snapshot => (setRoomDetails(snapshot.data())))
         }
+
+        db.collection("rooms")
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot((snapshot) => setRoomMessages(snapshot.docs.map((doc) => doc.data())))
     }, [roomId]);
+
     console.log(roomDetails)
+    console.log("Messages >>", roomMessages)
     return (
         <div className="chat">
             <div className="chat_header">
@@ -34,6 +44,17 @@ function Chat() {
 
                 </div>
             </div>
+            <div className="chat_messages">
+                {roomMessages.map(({message, timestamp, user, userImage}) => (
+                    <Message 
+                        message = {message}
+                        timestamp = {timestamp}
+                        user={user}
+                        userImage={userImage}
+                    />
+                ))}
+            </div>
+            <ChatInput channelName={roomDetails?.name} channelId={roomId} />
         </div>
     )
 }
